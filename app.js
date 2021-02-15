eb4f64ace3254aefd9de08a28b63a81e9376709b
asim-altayb <asomy0909@gmail.com>
2021-02-15 09:38:52 +0200

    click event

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log('hello, vue')
+const app = Vue.createApp({
+    data(){
+        return {
+            title: 'The Final Epimre',
+            author: 'Asim',
+            age: 29
+        }
+    },
+    methods: {
+        changeTitle(title){
+            this.title = title
+        }
+    }
+})
+
+app.mount('#app')
